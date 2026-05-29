4f035be3a69bbf0752e2a1c9aac2ebe2ee087e56
Matthew <58734642+mwwalrath@users.noreply.github.com>
2026-05-28 20:31:09 -0400

    Update test_aggregate_auditlog_sources.py

--- RevealX 360 Audit Log Source Aggregator/test_aggregate_auditlog_sources.py
+++ RevealX 360 Audit Log Source Aggregator/test_aggregate_auditlog_sources.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2026, ExtraHop Networks. All rights reserved.
+# Copyright (c) 2026, Matthew Walrath. All rights reserved.
 # Licensed under the BSD 2-Clause License. See LICENSE for details.
 """Tests for aggregate_auditlog_sources.
 
